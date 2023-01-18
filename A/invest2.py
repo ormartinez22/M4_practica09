9d3f30551c3fedadc1c9ee97e374ecba8207a7a8
AqeedatNabi <114878516+AqeedatNabi@users.noreply.github.com>
2023-01-18 20:59:13 +0100

    update A p9

---
+++ A/invest2.py
@@ -0,0 +1,10 @@
+"""
+Print the total accumulated money
+after 7 years, using variables
+"""
+savings=100
+increase=1.1
+result=savings*1.1
+fianlValue=result*7
+print(fianlValue)
+
---
+++ A/sum.py
@@ -0,0 +1,5 @@
+#Print the sum of 3 and 7 
+x=3
+y=7
+z=x+y
+print(z)
